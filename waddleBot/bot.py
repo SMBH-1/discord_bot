5f5e12a19fda67720b2a45ef40cbbfe72e456c19
sevastians1 <sevaschlau@hotmail.com>
2023-01-09 12:12:25 -0500

    corrected 'cannot send an empty emssage functionality'

--- waddleBot/bot.py
+++ waddleBot/bot.py
@@ -10,7 +10,8 @@ async def send_message(message, user_message, is_private):
     # Handle response and send in PM or channel
     try:
         response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        if response:
+            await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e: 
         print(e)
 
